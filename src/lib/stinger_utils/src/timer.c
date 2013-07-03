24c28faafc7f308d0fe1ca3b92dc25cf60457b12
Rob McColl <rob@robmccoll.com>
2013-07-03 11:25:07 -0400

    fixing timer for Mac - ugh apple come on

--- src/lib/stinger_utils/src/timer.c
+++ src/lib/stinger_utils/src/timer.c
@@ -3,8 +3,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-//#include "stinger-defs.h"
 #include "timer.h"
+
+#ifdef __APPLE__
+#include <CoreServices/CoreServices.h>
+#endif
 
 #ifdef _OPENMP
 #include <omp.h>
@@ -15,100 +18,121 @@
 #include <sys/mta_task.h>
 #include <machine/runtime.h>
 
-void
-init_timer (void)
-{
-  /* Empty. */
-}
+  void
+  init_timer (void)
+  {
+    /* Empty. */
+  }
 
-double
-timer (void)
-{
-  return ((double) mta_get_clock (0) / mta_clock_freq ());
-}
+  double
+  timer (void)
+  {
+    return ((double) mta_get_clock (0) / mta_clock_freq ());
+  }
 
-double
-timer_getres (void)
-{
-  /* A guess. */
-  return 1.0 / mta_clock_freq ();
-}
+  double
+  timer_getres (void)
+  {
+    /* A guess. */
+    return 1.0 / mta_clock_freq ();
+  }
 
 
 #elif defined(_OPENMP)
 
-void
-init_timer (void)
-{
-  /* Empty. */
-}
+  void
+  init_timer (void)
+  {
+    /* Empty. */
+  }
 
-double
-timer (void)
-{
-  return omp_get_wtime ();
-}
+  double
+  timer (void)
+  {
+    return omp_get_wtime ();
+  }
 
-double
-timer_getres (void)
-{
-  return omp_get_wtick ();
-}
+  double
+  timer_getres (void)
+  {
+    return omp_get_wtick ();
+  }
 
+#elif defined(__APPLE__)
+
+  void
+  init_timer (void)
+  {
+    /* Empty. */
+  }
+
+  double
+  timer (void)
+  {
+    UnsignedWide uw = AbsoluteToNanoseconds(UpTime());
+    double nano = ((((unsigned long long)uw.hi)<<32)|(uw.lo));
+    return (nano / (1e9));
+  }
+
+  double
+  timer_getres (void)
+  {
+    return 1e-9;
+  }
 
 #else /* Elsewhere */
 
-static clockid_t clockid;
+  static clockid_t clockid;
 
-#if defined(CLOCK_REALTIME_ID)
-#define CLKID CLOCK_REALTIME_ID
-#define CLKIDNAME "CLOCK_REALTIME_ID"
-#elif defined(CLOCK_THREAD_CPUTIME_ID)
-#define CLKID CLOCK_THREAD_CPUTIME_ID
-#define CLKIDNAME "CLOCK_THREAD_CPUTIME_ID"
-#elif defined(CLOCK_REALTIME_ID)
-#warning "Falling back to realtime clock."
-#define CLKID CLOCK_REALTIME_ID
-#define CLKIDNAME "CLOCK_REALTIME_ID"
-#else
-#error "Cannot find a clock!"
-#endif
+  #if defined(CLOCK_REALTIME_ID)
+  #define CLKID CLOCK_REALTIME_ID
+  #define CLKIDNAME "CLOCK_REALTIME_ID"
+  #elif defined(CLOCK_THREAD_CPUTIME_ID)
+  #define CLKID CLOCK_THREAD_CPUTIME_ID
+  #define CLKIDNAME "CLOCK_THREAD_CPUTIME_ID"
+  #elif defined(CLOCK_REALTIME_ID)
+  #warning "Falling back to realtime clock."
+  #define CLKID CLOCK_REALTIME_ID
+  #define CLKIDNAME "CLOCK_REALTIME_ID"
+  #else
+  #error "Cannot find a clock!"
+  #endif
 
-/**
-* @brief Initialize the system timer
-*/
-void
-init_timer (void)
-{
-  int err;
-  err = clock_getcpuclockid (0, &clockid);
-  if (err >= 0)
-    return;
-  fprintf (stderr, "Unable to find CPU clock, falling back to "
-           CLKIDNAME "\n");
-  clockid = CLKID;
-}
+  /**
+  * @brief Initialize the system timer
+  */
+  void
+  init_timer (void)
+  {
+    int err;
+    err = clock_getcpuclockid (0, &clockid);
+    if (err >= 0)
+      return;
+    fprintf (stderr, "Unable to find CPU clock, falling back to "
+	     CLKIDNAME "\n");
+    clockid = CLKID;
+  }
 
-double
-timer (void)
-{
-  struct timespec tp;
-  clock_gettime (clockid, &tp);
-  return (double) tp.tv_sec + 1.0e-9 * (double) tp.tv_nsec;
-}
+  double
+  timer (void)
+  {
+    struct timespec tp;
+    clock_gettime (clockid, &tp);
+    return (double) tp.tv_sec + 1.0e-9 * (double) tp.tv_nsec;
+  }
 
-/**
-* @brief Get the resolution of the system clock
-*
-* @return Clock Resolution
-*/
-double
-timer_getres (void)
-{
-  struct timespec tp;
-  clock_getres (clockid, &tp);
-  return (double) tp.tv_sec + 1.0e-9 * (double) tp.tv_nsec;
-}
+  /**
+  * @brief Get the resolution of the system clock
+  *
+  * @return Clock Resolution
+  */
+  double
+  timer_getres (void)
+  {
+    struct timespec tp;
+    clock_getres (clockid, &tp);
+    return (double) tp.tv_sec + 1.0e-9 * (double) tp.tv_nsec;
+  }
 
 #endif
 
