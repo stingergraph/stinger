8af5dd03beace4c3ab6293d16f34dac0b27e80b4
David Ediger <dediger@gatech.edu>
2013-07-23 14:49:48 -0400

    Streaming JSON parser...woohoo

---
+++ src/bin/clients/streams/json_stream/inc/json_stream.h
@@ -0,0 +1,11 @@
+#ifndef _JSON_STREAM_H
+#define _JSON_STREAM_H
+
+#include "stinger_net/proto/stinger-batch.pb.h"
+#include "stinger_net/send_rcv.h"
+
+using namespace gt::stinger;
+
+
+
+#endif /* _JSON_STREAM_H */
