30a187feb2a21b6e5b7ef1e0c92bb671229ce7e8
David Ediger <dediger@gatech.edu>
2013-08-28 17:38:17 -0400

    Integrating support for Doxygen.  Try make doc.  Resolves #16

---
+++ doc/main.h
@@ -0,0 +1,8 @@
+/**
+ * @mainpage STINGER Documentation
+ *
+ * @section intro-sec Introduction
+ * <a href="http://www.cc.gatech.edu/stinger">STINGER</a> documentation is maintained using 
+ * <a href="http://www.doxygen.org">Doxygen</a>.
+ *
+ */
