f0117edc8bee0fea28d9acd892fed6270958174d
Rob McColl <rob@robmccoll.com>
2013-10-02 17:28:04 -0400

    adding mon_handling to stinger_net

---
+++ src/lib/stinger_net/inc/mon_handling.h
@@ -0,0 +1,10 @@
+#ifndef  MON_HANDLING_H
+#define  MON_HANDLING_H
+
+#include <stdint.h>
+
+extern "C" int64_t
+mon_connect(int port, const char * host, const char * name);
+
+
+#endif  /*MON_HANDLING_H*/
