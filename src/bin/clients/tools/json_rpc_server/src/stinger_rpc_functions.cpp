0c5d54989c8be4a522c36f4f7ef69a06f25ea745
Rob McColl <rob@robmccoll.com>
2013-10-08 21:41:10 -0400

    adding edge and vertex type counts / arrays to stats rpc call

--- src/bin/clients/tools/json_rpc_server/src/stinger_rpc_functions.cpp
+++ src/bin/clients/tools/json_rpc_server/src/stinger_rpc_functions.cpp
@@ -132,13 +132,49 @@ JSON_RPC_request::operator()(rapidjson::Value * params, rapidjson::Value & resul
 int64_t 
 JSON_RPC_get_graph_stats::operator()(rapidjson::Value * params, rapidjson::Value & result, rapidjson::MemoryPoolAllocator<rapidjson::CrtAllocator> & allocator)
 {
-  rapidjson::Value nv;
-  rapidjson::Value ne;
+  bool get_types;
+  rpc_params_t p[] = {
+    {"get_types", TYPE_BOOL, &get_types, true, 0},
+    {NULL, TYPE_NONE, NULL, false, 0}
+  };
+
+  if (!contains_params(p, params)) {
+    return json_rpc_error(-32602, result, allocator);
+  }
 
   stinger_t * S = server_state->get_stinger();
   if (!S) {
     LOG_E ("STINGER pointer is invalid");
     return json_rpc_error(-32603, result, allocator);
+  }
+
+  rapidjson::Value nv;
+  rapidjson::Value ne;
+  rapidjson::Value edge_types;
+  rapidjson::Value vertex_types;
+
+  /* default: counts of types, otherwise: lists of type strings */
+  int64_t num_edge_types = stinger_etype_names_count(S);
+  int64_t num_vertex_types = stinger_vtype_names_count(S);
+  if(!get_types) {
+    edge_types.SetInt64(num_edge_types);
+    vertex_types.SetInt64(num_vertex_types);
+  } else {
+    rapidjson::Value type_name;
+
+    edge_types.SetArray();
+    for(int64_t i = 0; i < num_edge_types; i++) {
+      char * type_name_str = stinger_etype_names_lookup_name(S,i);
+      type_name.SetString(type_name_str,strlen(type_name_str),allocator);
+      edge_types.PushBack(type_name, allocator);
+    }
+
+    vertex_types.SetArray();
+    for(int64_t i = 0; i < num_vertex_types; i++) {
+      char * type_name_str = stinger_vtype_names_lookup_name(S,i);
+      type_name.SetString(type_name_str,strlen(type_name_str),allocator);
+      vertex_types.PushBack(type_name, allocator);
+    }
   }
 
   int64_t num_vertices = stinger_mapping_nv(S);
@@ -148,6 +184,8 @@ JSON_RPC_get_graph_stats::operator()(rapidjson::Value * params, rapidjson::Value
 
   result.AddMember("vertices", nv, allocator);
   result.AddMember("edges", ne, allocator);
+  result.AddMember("edge_types", edge_types, allocator);
+  result.AddMember("vertex_types", vertex_types, allocator);
 
   return 0;
 }
