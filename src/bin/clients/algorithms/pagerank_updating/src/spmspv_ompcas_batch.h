1859aa6bfa989b699a6b0f991c8df282bb2f15b5
Jason Riedy <jason.riedy@cc.gatech.edu>
2014-07-14 09:25:34 -0400

    Forgot degscaled in ompcas_batch declarations...

--- src/bin/clients/algorithms/pagerank_updating/src/spmspv_ompcas_batch.h
+++ src/bin/clients/algorithms/pagerank_updating/src/spmspv_ompcas_batch.h
@@ -22,6 +22,16 @@ stinger_unit_dspmTv_ompcas_batch (const int64_t nv,
                             const double beta, double * y);
 
 void
+stinger_dspmTv_degscaled_ompcas_batch (const int64_t nv,
+                       const double alpha, const struct stinger *S, const double * x,
+                       const double beta, double * y);
+
+void
+stinger_unit_dspmTv_degscaled_ompcas_batch (const int64_t nv,
+                            const double alpha, const struct stinger *S, const double * x,
+                            const double beta, double * y);
+
+void
 stinger_dspmTspv_ompcas_batch (const int64_t nv,
                          const double alpha, const struct stinger *S,
                          const int64_t x_deg, const int64_t * x_idx, const double * x_val,
@@ -31,6 +41,22 @@ stinger_dspmTspv_ompcas_batch (const int64_t nv,
 
 void
 stinger_unit_dspmTspv_ompcas_batch (const int64_t nv,
+                              const double alpha, const struct stinger *S,
+                              const int64_t x_deg, const int64_t * x_idx, const double * x_val,
+                              const double beta,
+                              int64_t * y_deg, int64_t * y_idx, double * y_val,
+                              int64_t * loc_ws, double * val_ws);
+
+void
+stinger_dspmTspv_degscaled_ompcas_batch (const int64_t nv,
+                         const double alpha, const struct stinger *S,
+                         const int64_t x_deg, const int64_t * x_idx, const double * x_val,
+                         const double beta,
+                         int64_t * y_deg, int64_t * y_idx, double * y_val,
+                         int64_t * loc_ws, double * val_ws);
+
+void
+stinger_unit_dspmTspv_degscaled_ompcas_batch (const int64_t nv,
                               const double alpha, const struct stinger *S,
                               const int64_t x_deg, const int64_t * x_idx, const double * x_val,
                               const double beta,
