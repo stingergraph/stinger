b4aabecc31290bc061faf233d9d33af9d66ec33c
David Ediger <dediger@gatech.edu>
2014-11-10 16:58:57 -0500

    Adding a switch to enable/disable reporting of edges incident to the source.

--- src/bin/clients/tools/json_rpc_server/src/egonet.cpp
+++ src/bin/clients/tools/json_rpc_server/src/egonet.cpp
@@ -20,6 +20,7 @@ JSON_RPC_egonet::operator()(rapidjson::Value * params, rapidjson::Value & result
   bool get_types;
   bool get_etypes;
   bool get_vtypes;
+  bool incident_edges;
 
   rpc_params_t p[] = {
     {"source", TYPE_VERTEX, &source, false, 0},
@@ -27,6 +28,7 @@ JSON_RPC_egonet::operator()(rapidjson::Value * params, rapidjson::Value & result
     {"get_types", TYPE_BOOL, &get_types, true, 0},
     {"get_etypes", TYPE_BOOL, &get_etypes, true, 0},
     {"get_vtypes", TYPE_BOOL, &get_vtypes, true, 0},
+    {"incident_edges", TYPE_BOOL, &incident_edges, true, 0},
     {NULL, TYPE_NONE, NULL, false, 0}
   };
 
@@ -49,6 +51,7 @@ JSON_RPC_egonet::operator()(rapidjson::Value * params, rapidjson::Value & result
   rapidjson::Value edges_str(rapidjson::kArrayType);
   rapidjson::Value vtx(rapidjson::kArrayType);
   rapidjson::Value vtx_str(rapidjson::kArrayType);
+  rapidjson::Value center, center_str;
   rapidjson::Value src, dst;
   rapidjson::Value src_str, dst_str;
   rapidjson::Value val(rapidjson::kArrayType);
@@ -70,12 +73,31 @@ JSON_RPC_egonet::operator()(rapidjson::Value * params, rapidjson::Value & result
     marks[i] = 0;
   }
 
+  /* mark and insert source if we are returning incident edges */
+  if (incident_edges) {
+    marks[source] = 1;
+    center.SetInt64(source);
+    vtx.PushBack(center, allocator);
+    
+    if (strings) {
+      char * physID;
+      uint64_t len;
+      if (-1 == stinger_mapping_physid_direct(S, source, &physID, &len)) {
+	physID = (char *) "";
+	len = 0;
+      }
+      center_str.SetString(physID, len, allocator);
+      vtx_str.PushBack(center_str, allocator);
+    }
+  }
+
   /* mark all neighbors of the source vertex */
   STINGER_FORALL_EDGES_OF_VTX_BEGIN (S, source) {
     int64_t u = STINGER_EDGE_DEST;
     marks[u] = 1;
     src.SetInt64(u);
     vtx.PushBack(src, allocator);
+
     if (strings) {
       char * physID;
       uint64_t len;
@@ -86,26 +108,48 @@ JSON_RPC_egonet::operator()(rapidjson::Value * params, rapidjson::Value & result
       src_str.SetString(physID, len, allocator);
       vtx_str.PushBack(src_str, allocator);
     }
+
+    if (incident_edges) {
+      src.SetInt64(source);
+      dst.SetInt64(u);
+      val.SetArray();
+      val.PushBack(src, allocator);
+      val.PushBack(dst, allocator);
+      edges.PushBack(val, allocator);
+
+      if (strings) {
+	char * physID;
+	uint64_t len;
+	if (-1 == stinger_mapping_physid_direct(S, source, &physID, &len)) {
+	  physID = (char *) "";
+	  len = 0;
+	}
+	src_str.SetString(physID, len, allocator);
+	
+	if (-1 == stinger_mapping_physid_direct(S, u, &physID, &len)) {
+	  physID = (char *) "";
+	  len = 0;
+	}
+	dst_str.SetString(physID, len, allocator);
+
+	val.SetArray();
+	val.PushBack(src_str, allocator);
+	val.PushBack(dst_str, allocator);
+	edges_str.PushBack(val, allocator);
+      }
+    }
+
   } STINGER_FORALL_EDGES_OF_VTX_END();
 
   /* now we will find all of the edges in common between the neighbors */
   STINGER_FORALL_EDGES_OF_VTX_BEGIN (S, source) {
     int64_t u = STINGER_EDGE_DEST;
-    src.SetInt64(u);
 
     STINGER_FORALL_EDGES_OF_VTX_BEGIN (S, u) {
       int64_t v = STINGER_EDGE_DEST;
-      if (strings) {
-	char * physID;
-	uint64_t len;
-	if (-1 == stinger_mapping_physid_direct(S, u, &physID, &len)) {
-	  physID = (char *) "";
-	  len = 0;
-	}
-	src_str.SetString(physID, len, allocator);
-      }
 
       if (marks[v]) {
+	src.SetInt64(u);
 	dst.SetInt64(v);
 	val.SetArray();
 	val.PushBack(src, allocator);
@@ -115,6 +159,12 @@ JSON_RPC_egonet::operator()(rapidjson::Value * params, rapidjson::Value & result
 	if (strings) {
 	  char * physID;
 	  uint64_t len;
+	  if (-1 == stinger_mapping_physid_direct(S, u, &physID, &len)) {
+	    physID = (char *) "";
+	    len = 0;
+	  }
+	  src_str.SetString(physID, len, allocator);
+
 	  if (-1 == stinger_mapping_physid_direct(S, v, &physID, &len)) {
 	    physID = (char *) "";
 	    len = 0;
