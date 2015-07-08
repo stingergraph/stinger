8c1e72d0a89ae86d1dbaa811573a4f3b82cbc3bc
Trevor Goodyear <trevor.goodyear@gtri.gatech.edu>
2015-07-08 17:33:25 -0400

    Add testing script for Flask API. Improvements to batch management for Flask API endpoint that provides edge insertion.

--- flask/stinger_flask.py
+++ flask/stinger_flask.py
@@ -1,55 +1,60 @@
-import sys
-import os
-import time
-import requests
-import json
-import threading
-import signal
+import sys, os, time, requests, json, threading, signal, logging, argparse, traceback
 from flask import Flask, request, jsonify
-import logging
-import argparse
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
+logging.basicConfig(stream=sys.stderr)
 
-sys.path.append("/path/to/stinger/src/py/")
-os.environ['STINGER_LIB_PATH'] = "/path/to/stinger/build/lib/"
+sys.path.append("/home/tgoodyear/projects/blueprints/src/py/")
+os.environ['STINGER_LIB_PATH'] = "/home/tgoodyear/projects/blueprints/build/lib/"
 
 import stinger.stinger_net as sn
 import stinger.stinger_core as sc
 
-TIMEOUT_SECS = 5
-BATCH_THRESHOLD = 1000
+# Value of -1 in either field disables its use as a threshold
+TIMEOUT_SECS = 1
+BATCH_THRESHOLD = 500
+
+STINGER_HOST = 'localhost'
+STINGER_PORT = '8088'
 
 
 app = Flask(__name__)
 
 @app.route('/insert', methods=['POST'])
 def insert():
+  global q
+  global cur
+  global con
+
   exec_time = time.time()
   if not request.json:
-    requests.abort(400)
+    abort(400)
 
   # grab the lock
   counter_lock.acquire()
 
   try:
     data = request.json
+    print len(data['edges']), "Edges received"
     if isinstance(data["edges"], list):
       edges = data["edges"]
       for x in edges:
-	try:
-	  source = x["src"]
-	  destination = x["dest"]
-	  edge_type = x["type"]
-	  timestamp = x["time"]
-	  s.add_insert(source, destination, edge_type, ts=timestamp)
-	except:
-	  pass
+        try:
+          source = int(x["src"])
+          destination = int(x["dest"])
+          edge_type = x["type"]
+          timestamp = int(x["time"])
+          s.add_insert(source, destination, edge_type, ts=timestamp)
+          # print "added edge", source, destination, edge_type, timestamp
+        except Exception as e:
+          print(traceback.format_exc())
+          pass
 
     # send immediately if the batch is now large
     current_batch_size = s.insertions_count + s.deletions_count
     if current_batch_size > BATCH_THRESHOLD:
       s.send_batch()
+      print "Sending batch of size", current_batch_size
 
   except:
     return jsonify({"error": "invalid input"})
@@ -58,40 +63,56 @@ def insert():
   finally:
     counter_lock.release()
   exec_time = time.time() - exec_time
-  return jsonify({"status": "success", "time": exec_time}), 201
+  return jsonify({"status": "success", "time": exec_time, "size": current_batch_size}), 201
+
 
 @app.route('/jsonrpc', methods=['POST'])
 def stinger_proxy():
-  payload = request.json
+  return stingerRPC(request.json)
+
+@app.route('/algorithms', methods=['GET'])
+def get_algs():
+  return stingerRPC({"jsonrpc": "2.0", "method": "get_algorithms", "id": 1})
+
+@app.route('/health', methods=['GET'])
+def stinger_health():
+  return stingerRPC({"jsonrpc": "2.0", "method": "get_server_health", "id": 1})
+
+def stingerRPC(payload):
   try:
-    r = requests.post('http://localhost:8088/jsonrpc', data=json.dumps(payload))
+    r = requests.post('http://'+ STINGER_HOST +':'+ STINGER_PORT +'/jsonrpc', data=json.dumps(payload))
   except:
+    print(traceback.format_exc())
     return jsonify({"error": "JSON-RPC down"})
   return r.text
+
 
 #
 # Initialize the connection to STINGER server
 #
 def connect(directed=False):
   global s
-  s = sn.StingerStream('localhost', 10102, directed)
+  s = sn.StingerStream(STINGER_HOST, 10102, directed)
 
   global counter_lock
   counter_lock = threading.Lock()
 
 #
-# attempt to send a batch every 60 seconds
+# attempt to send a batch every TIMEOUT_SECS seconds
 #
 def sendBatch():
   if s.insertions_count > 0 or s.deletions_count > 0:
     counter_lock.acquire()
     try:
       s.send_batch()
+      print "Sending batch with", s.insertions_count, "insertions and", s.deletions_count, "deletions"
     finally:
       counter_lock.release()
-  global t
-  t = threading.Timer(TIMEOUT_SECS, sendBatch)
-  t.start()
+
+  if TIMEOUT_SECS > -1:
+      global t
+      t = threading.Timer(TIMEOUT_SECS, sendBatch)
+      t.start()
 
 def signal_handler(signal, frame):
   try:
@@ -100,15 +121,29 @@ def signal_handler(signal, frame):
     pass
   sys.exit(0)
 
+
+if not 's' in globals():
+  print 'Connecting to STINGER'
+  try:
+    connect()
+  except e as Exception:
+    print str(e)
+  print 'Connected to STINGER'
+if not 't' in globals():
+  print 'STINGER timer setup started'
+  try:
+    sendBatch()
+  except e as Exception:
+    print str(e)
+  print 'STINGER timer setup finished'
 #
 # main
 #
 if __name__ == '__main__':
-  signal.signal(signal.SIGINT, signal_handler)
-  parser = argparse.ArgumentParser(description="STINGER Flask Relay Server")
-  parser.add_argument('--directed', action="store_true")
-  args = parser.parse_args()
-  connect(args.directed)
-  sendBatch()
-  app.run()
-
+    signal.signal(signal.SIGINT, signal_handler)
+    parser = argparse.ArgumentParser(description="STINGER Flask Relay Server")
+    parser.add_argument('--directed', action="store_true")
+    args = parser.parse_args()
+    #connect(args.directed)
+    sendBatch()
+    app.run(debug=True)
---
+++ flask/test_flask.py
@@ -0,0 +1,31 @@
+import requests, argparse, numpy, json, time
+from time import gmtime, strftime
+
+TEST_URL = 'http://localhost:5000/insert'
+
+parser = argparse.ArgumentParser(description="Test script for STINGER Flask Relay Server")
+parser.add_argument('--batch_size', help="Number of edges to send per batch", default=1000, type=int)
+parser.add_argument('--batch_rate', help="Seconds between sending a batch", default=5, type=int)
+parser.add_argument('--edge_types', help="Number of different edge types", default=2, type=int)
+args = parser.parse_args()
+
+while True:
+    edges = []
+    src = numpy.random.randint(0,args.batch_size,args.batch_size)
+    dst = numpy.random.randint(0,args.batch_size,args.batch_size)
+    typ = numpy.random.randint(0,args.edge_types,args.batch_size)
+    exec_time = time.time()
+    for i in range(0,args.batch_size):
+        e = {'src':str(src[i]),'dest':str(dst[i]),'type':str(typ[i]),'time':int(strftime("%Y%m%d%H%M%S", gmtime()))}
+        edges.append(e)
+
+    payload = {'edges':edges}
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    try:
+        r = requests.post(TEST_URL, data=json.dumps(payload), headers=headers)
+        print r.text
+    except:
+        print "Unable to reach", TEST_URL, "Sleeping for 5 seconds"
+        time.sleep(5)
+    while time.time() < exec_time + args.batch_rate:
+        time.sleep(1)
