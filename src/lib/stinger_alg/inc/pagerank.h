b2c4c17072288c5d137e6ad2ea86ebfb1b9b4833
Jason Poovey <japoovey@gmail.com>
2015-07-17 09:27:38 -0400

    Pulling out pagerank to stinger_alg

--- src/lib/stinger_alg/inc/pagerank.h
+++ src/lib/stinger_alg/inc/pagerank.h
@@ -1,3 +1,6 @@
+#ifndef STINGER_PAGERANK_H_
+#define STINGER_PAGERANK_H_
+
 #include "stinger_core/stinger.h"
 #include "stinger_core/xmalloc.h"
 #include "stinger_core/stinger_error.h"
@@ -8,10 +11,13 @@
 #define DAMPINGFACTOR_DEFAULT 0.85
 #define MAXITER_DEFAULT 20
 
-double * set_tmp_pr(double * tmp_pr_in, int64_t NV);
-void unset_tmp_pr(double * tmp_pr, double * tmp_pr_in);
-
 int64_t page_rank_directed(stinger_t * S, int64_t NV, double * pr, double * tmp_pr_in, double epsilon, double dampingfactor, int64_t maxiter);
 int64_t page_rank (stinger_t * S, int64_t NV, double * pr, double * tmp_pr_in, double epsilon, double dampingfactor, int64_t maxiter);
 int64_t page_rank_type(stinger_t * S, int64_t NV, double * pr, double * tmp_pr_in, double epsilon, double dampingfactor, int64_t maxiter, int64_t type);
 int64_t page_rank_type_directed(stinger_t * S, int64_t NV, double * pr, double * tmp_pr_in, double epsilon, double dampingfactor, int64_t maxiter, int64_t type);
+
+
+double * set_tmp_pr(double * tmp_pr_in, int64_t NV);
+void unset_tmp_pr(double * tmp_pr, double * tmp_pr_in);
+
+#endif
