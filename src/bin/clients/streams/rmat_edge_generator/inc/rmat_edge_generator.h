d7ad430cc1341e6c4836eccc878ce0dd396a123e
Jason Poovey <japoovey@gmail.com>
2015-07-22 18:00:48 -0400

    Adding the Pagerank_test to test performance

--- src/bin/clients/streams/rmat_edge_generator/inc/rmat_edge_generator.h
+++ src/bin/clients/streams/rmat_edge_generator/inc/rmat_edge_generator.h
@@ -7,8 +7,4 @@
 using namespace gt::stinger;
 
 
-void
-rmat_edge (int64_t * iout, int64_t * jout,
-           int SCALE, double A, double B, double C, double D, dxor128_env_t * env);
-
 #endif /* _RMAT_EDGE_GENERATOR_H */
