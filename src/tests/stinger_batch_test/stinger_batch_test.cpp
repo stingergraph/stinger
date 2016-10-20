76b1f103b2e4e6c87f70825064304b2a8e965f2c
Eric Hein <ehein6@gatech.edu>
2016-10-20 15:25:00 -0400

    Added more intensive test.

--- src/tests/stinger_batch_test/stinger_batch_test.cpp
+++ src/tests/stinger_batch_test/stinger_batch_test.cpp
@@ -90,6 +90,69 @@ TEST_F(StingerBatchTest, simple_batch_insertion) {
     EXPECT_EQ(consistency,0);
 }
 
+TEST_F(StingerBatchTest, batch_insertion) {
+    int64_t total_edges = 0;
+    int64_t consistency;
+
+    // Create batch to insert
+    std::vector<stinger_edge_update> updates;
+    for (int i = 0; i < 100; ++i)
+    {
+        stinger_edge_update u = {
+            0, NULL, // type, type_str
+            i, NULL, // source, source_str
+            i+1, NULL, // destination, destination_str
+            1, // weight
+            i*100, // time
+            0, 0 // result, meta_index
+        };
+        updates.push_back(u);
+        stinger_edge_update v = {
+            0, NULL, // type, type_str
+            i+1, NULL, // source, source_str
+            i, NULL, // destination, destination_str
+            1, // weight
+            i*100, // time
+            0, 0 // result, meta_index
+        };
+        updates.push_back(v);
+    }
+
+    // Do the updates
+    stinger_batch_update(S, updates, EDGE_WEIGHT_INCR);
+
+    // Make sure all edges were detected as "new"
+    for (update_iterator u = updates.begin(); u != updates.end(); ++u)
+    {
+        EXPECT_EQ(u->result, 1);
+    }
+
+    consistency = stinger_consistency_check(S,S->max_nv);
+    EXPECT_EQ(consistency,0);
+
+    // Reset return codes so we can reuse this batch
+    OMP("omp parallel for")
+    for (update_iterator u = updates.begin(); u < updates.end(); ++u) { u->result = 0; }
+
+    // Insert same batch again
+    stinger_batch_update(S, updates, EDGE_WEIGHT_INCR);
+
+    // Make sure all edges were detected as "updated"
+    for (update_iterator u = updates.begin(); u != updates.end(); ++u)
+    {
+        EXPECT_EQ(u->result, 0);
+    }
+
+    // Make sure all edge weights were incremented properly
+    STINGER_FORALL_EDGES_OF_ALL_TYPES_BEGIN(S) {
+        EXPECT_EQ(STINGER_EDGE_WEIGHT, 2);
+    }STINGER_FORALL_EDGES_OF_ALL_TYPES_END();
+
+    consistency = stinger_consistency_check(S,S->max_nv);
+    EXPECT_EQ(consistency,0);
+
+}
+
 
 int
 main (int argc, char *argv[])
