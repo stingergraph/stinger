ed250cf755f98a32c7de85f7325bb2dd21e073d7
Eric Hein <ehein6@gatech.edu>
2016-11-22 14:35:28 -0500

    Update stale comments.

--- lib/stinger_core/src/x86_full_empty.c
+++ lib/stinger_core/src/x86_full_empty.c
@@ -1,13 +1,10 @@
-/* x86 Emulation of Full Empty Bits Using atomic check-and-swap.
+/* Emulation of Full Empty Bits Using atomic check-and-swap.
  *
  * NOTES:
  * - Using these functions means that the MARKER value defined
  *   below must be reserved in your application and CANNOT be
  *   considered a normal value.  Feel free to change the value to
  *   suit your application.
- * - Do not compile this file with optimization.  There are loops
- *   that do not make sense in a serial context.  The compiler will
- *   optimize them out.
  * - Improper use of these functions can and will result in deadlock.
  *
  * author: rmccoll3@gatech.edu
